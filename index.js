7ef7f1084b33567b1b5a4cb66bcf3571a2e63ed1
Rashedul Hasan <Rashedul Hasan>
2021-09-19 17:48:47 +0600

    JavaScript-Practice-01

---
+++ index.js
@@ -0,0 +1,91 @@
+// console.log("Hello World");
+
+// var value = 14;
+// console.log(value);
+
+// var value = 14.45;
+// console.log(typeof value);
+
+// var myName = 'tapu';
+// console.log(myName);
+
+// var isGood = true;
+// console.log(isGood);
+
+// var isBad = false;
+// console.log(isBad);
+
+// var isGood = true;
+// console.log(typeof isGood);
+
+// var love = "I Love JavaScript"
+// console.log(love);
+// console.log(love.toLocaleUpperCase());
+// console.log(love.toLocaleLowerCase());
+// console.log(love.indexOf('Love'));
+// console.log(love.split(" "));
+
+
+// var num1 = 10;
+// var num2 = 12.5;
+// num2 = parseInt(num2);
+// console.log(num1+num2);
+// num3 = '15.234'
+// num3 = parseFloat(num3);
+// console.log(num3);
+
+// var num = 2;
+// num++;
+// num--;
+// console.log(num)
+
+// var num1 = -5;
+// var num1 = Math.abs(num1);
+// console.log(num1);
+// var num2 = 6.54545;
+// var num2 = Math.round(num2);
+// console.log(num2);
+// var num3 = 6.56545;
+// var num3 = Math.floor(num3);
+// console.log(num3);
+// var num4 = 6.525;
+// var num4 = Math.ceil(num4);
+// console.log(num4);
+
+// var price1 = 10;
+// var price2 = 3;
+// var add = price1+price2;
+// var sub = price1-price2;
+// var multi = price1*price2;
+// var div = price1/price2;
+// var rem = price1%price2;
+// console.log(add,sub,multi,div,rem);
+
+// var num = 15;
+// if(num>5){
+//     console.log(num,"is greater then 5")
+// }
+// else {
+//     console.log(num,"is not greater then 5")
+// }
+
+
+// x = 10;
+// y = 4;
+// if(x==10 && y==5){
+//     console.log("Gora gori korta jaba-1");
+// }
+// else if(x==10 ||y==10){
+//     console.log("Gora gori korta jaba-2");
+// }
+// else{
+//     console.log("No gora gori")
+// }
+
+
+
+
+
+
+
+
